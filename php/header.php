075b6c2f993dd315d2d60c2646654abe71777b45
William Pires <william.wpires@gmail.com>
2020-02-21 16:11:14 +0100

    démarrage dossier

---
+++ php/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- META -->
+    <meta charset="UTF-8">
+    <meta name="author" content="William Pires">
+    <meta name="description" content="Site My Cave">
+    <meta name="keywords" content="collection de bouteilles de vin, web development">
+    <meta name="robots" content="index, follow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3">
+
+    <!-- OPEN GRAPH ogp.me -->
+    <meta property="og:title" content="My Cave">
+    <meta property="og:description" content="Collection de bouteilles de vin">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+    <!-- LINKS -->
+    <link rel="stylesheet" href="assets/css/nomarge.css">
+    <link rel="stylesheet" href="assets/css/style.min.css">
+    <title>My Cave</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    </header>
+
+
+
+
+    <script type="text/javascript" src="assets/libs/jquery/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/src/script.js"></script>
+</body>
+</html>
