fc21a7c79fc48e6285e5a2504158c4b9bd651ec7
William Pires <william.wpires@gmail.com>
2020-03-25 18:31:39 +0100

    begin update

---
+++ request/update_post.php
@@ -0,0 +1,63 @@
+<?php 
+
+include 'connect.php';
+
+$req = $bdd->prepare('
+
+    UPDATE main_bottle
+    SET name=?, region=?, country=?, image=?, grape=?, year=?, description=?
+    WHERE id=?
+
+    ');
+
+$req->execute(array(
+    $_POST['name'],
+    $_POST['region'],
+    $_POST['country'],
+    $_POST['image'],
+    $_POST['grape'],
+    $_POST['year'],
+    $_POST['description'],
+    $_GET['id_url']
+));
+
+$donnees= $req->fetch();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
